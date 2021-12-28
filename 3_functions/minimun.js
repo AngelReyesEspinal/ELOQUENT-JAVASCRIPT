bd49afdb2bbd64e44d9587b5538ddb20dacfaa87
Angel Arsenio Reyes Espinal <angel.reyes@solvex.com.do>
2021-12-28 00:19:39 -0400

    fix

--- 3_functions/minimun.js
+++ 3_functions/minimun.js
@@ -1,7 +1,8 @@
 const getMinimunNumber = (n1, n2) => {
-    return Math.min(n1, n2);
+    if (n1 < n2) 
+        return n1;
+    return n2;
 }
 
 const minimunNumber = getMinimunNumber(3, 2);
-
 console.log(minimunNumber);
